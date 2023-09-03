79ed7635a558036e6f72947f6abce20fe80771e4
jrayoub <aitounaayoub05@gmail.com>
2023-09-03 11:12:28 +0100

    fixed mariadb script

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -1,22 +1,30 @@
 service mariadb start;
 echo "✔ Started server \n"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;";
+
+mysql_secure_installation <<eof
+
+y
+y
+${MYSQL_ROOT_PASSWORD}
+${MYSQL_ROOT_PASSWORD}
+y
+y
+y
+y
+eof
+
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;";
 echo "✔ CREATE DATABASE\n";
 
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';";
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';";
 echo "✔ CREATE USER ${MYSQL_USER}\n";
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';";
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';";
 echo "✔ GRANT ALL PRIVILEGES TO USER ${MYSQL_USER}\n";
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-echo "✔ ALTER USER 'root'@'localhost' \n";
 
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;";
 echo "✔ FLUSH PRIVILEGES;\n";
-
-
 
 mysqladmin  -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown ;
 echo "✔ shutdown\n";
