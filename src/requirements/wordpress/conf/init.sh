57e1bde03d778a910ce91c2e39388b8a6db61628
Ayoub Aitouna <aaitouna@c3r7p6.1337.ma>
2023-09-06 05:48:29 +0100

    changes

--- src/requirements/wordpress/conf/init.sh
+++ src/requirements/wordpress/conf/init.sh
@@ -1,5 +1,5 @@
-sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', $MYSQL_DATABASE);/" wp-config.php;
-sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', $MYSQL_USER);/" wp-config.php;
-sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', $MYSQL_PASSWORD);/" wp-config.php;
-sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', $MYSQL_HOST);/" wp-config.php;
+sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '$MYSQL_DATABASE' );/" wordpress/wp-config.php
+sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER');/" wordpress/wp-config.php;
+sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$MYSQL_PASSWORD');/" wordpress/wp-config.php;
+sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306');/" wordpress/wp-config.php;
 tail -f /dev/null
