fe74c1cf3f642a3530b66f951e1b9f4fa0efa00c
Ayoub Aitouna <aaitouna@e1r3p1.1337.ma>
2023-10-12 16:34:40 +0100

    chanegs

--- src/requirements/wordpress/tools/config.sh
+++ src/requirements/wordpress/tools/config.sh
@@ -6,30 +6,23 @@ sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '$MYSQL_USER'
 sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '$MYSQL_PASSWORD');/" /var/www/html/wp-config.php;
 sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306');/" /var/www/html/wp-config.php;
 
-# echo "define('WP_CACHE', true);" >> /var/www/html/wp-config.php
-# echo "define('WP_REDIS_HOST', 'redis');" >> /var/www/html/wp-config.php
-# echo "define('WP_REDIS_PORT', '6379');" >> /var/www/html/wp-config.php
-
 wp core install --allow-root \
     --url=localhost \
     --title="My New Site" \
     --admin_user=$WP_ADMIN \
     --admin_password=$WP_ADMIN_PASSOWRD \
-    --admin_email="aaitouna@student.1337.ma"
+    --admin_email="aaitouna@student.1337.ma";
 
-wp user create --allow_root \
-    $WP_USER \
-    "aaitouna@student.1337.ma" \
-    --user_pass=$WP_USER_PASSOWRD
+wp user create --allow-root $WP_USER "aaitouna@student.com" --user_pass=$WP_USER_PASSOWRD ;
 
-wp plugin install redis-cache --activate --allow-root
+wp plugin install redis-cache --activate --allow-root ;
 
-wp config set --allow-root WP_REDIS_CLIENT "phpredis"
-wp config set --allow-root WP_REDIS_HOST "redis"
-wp config set --allow-root WP_REDIS_PORT "6379"
-wp config set --allow-root WP_REDIS_PREFIX "wp_"
+wp config set --allow-root WP_REDIS_CLIENT "phpredis" ;
+wp config set --allow-root WP_REDIS_HOST "redis" ;
+wp config set --allow-root WP_REDIS_PORT "6379" ;
+wp config set --allow-root WP_REDIS_PREFIX "wp_" ;
 
-wp plugin update --all --allow-root
-wp redis enable --allow-root
+wp plugin update --all --allow-root ;
+wp redis enable --allow-root ;
 
 exec $@
